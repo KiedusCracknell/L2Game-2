536c4e7bb4fb1e2227dc67ee0482cc77848429f2
KiedusCracknell <62589687+KiedusCracknell@users.noreply.github.com>
2022-09-11 18:40:05 +1200

    added some global vars in main.js for later use

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 canvas.width = 600;
 canvas.height = 400;
+
+let spacePressed = false; //true if space is pressed
+let angle = 0; //used to calculate angle of player for use in movement
+let hue = 0; //colours
+let frame = 0; //keeps track of frame count in animation, mainly used to set interval in which obstacles appear
+let score = 0; //Will increase when player avoids obstacles
+let gamespeed = 2; //speed at which the obstacles, background etc. move at, can be changed by difficulty settings
+
