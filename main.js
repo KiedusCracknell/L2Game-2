b781c16788aa4aaec2de9227f4b2bf73ec900586
KiedusCracknell <62589687+KiedusCracknell@users.noreply.github.com>
2022-09-13 23:09:39 +1200

    added animate function in main.js to clear canvas at the end of frames

--- main.js
+++ main.js
@@ -8,5 +8,9 @@ let angle = 0; //used to calculate angle of player for use in movement
 let hue = 0; //colours
 let frame = 0; //keeps track of frame count in animation, mainly used to set interval in which obstacles appear
 let score = 0; //Will increase when player avoids obstacles
-let gamespeed = 2; //speed at which the obstacles, background etc. move at, can be changed by difficulty settings
+let gamespeed = 2; //speed at which the obstacles, background etc. move at, can be changed by difficulty settings. also allows for paralax effects to be made easily
 
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    requestAnimationFrame(animate);
+} //clears canvas at end of every frame, recurs by calling itself at end of the function
