e6d5eb0641199a569e33193105dccac153ea7b52
KiedusCracknell <62589687+KiedusCracknell@users.noreply.github.com>
2022-09-13 23:16:35 +1200

    added rectangle to represent the player temporarily

--- main.js
+++ main.js
@@ -12,5 +12,6 @@ let gamespeed = 2; //speed at which the obstacles, background etc. move at, can 
 
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(10, 10, 50, 50);
     requestAnimationFrame(animate);
 } //clears canvas at end of every frame, recurs by calling itself at end of the function
