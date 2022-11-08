22536ab2929aec6cf22ed2b57de1ae609a20fd4c
KiedusCracknell <62589687+KiedusCracknell@users.noreply.github.com>
2022-11-08 13:48:47 +1300

    created custom draw method for obstacles

--- obstacles.js
+++ obstacles.js
@@ -1,11 +1,16 @@
 const obstaclesArray = [];
 
 class Obstacle {
-    constructor(){
-        this.top = (Math.random() * canvas.height/3) + 20; //height of top obstacle, random number between 0 and canvbas.height/3
-        this.bottom = (Math.random() * canvas.height/3) + 20; //height of bottom obstacle
+    constructor() {
+        this.top = (Math.random() * canvas.height / 3) + 20; //height of top obstacle, random number between 0 and canvbas.height/3
+        this.bottom = (Math.random() * canvas.height / 3) + 20; //height of bottom obstacle
         this.x = canvas.width; //spawns obstacles just offscreen to the right
         this.width = 20; // width of the obstacles
-        this.color = 'hsl(' + hue + '100%, 50%';
+        this.color = 'hsl(' + hue + '100%, 50%'; //color of the obstacles, rainbow
+    }
+    draw(){
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, 0, this.width, this.top); // draws the top obstacles
+        ctx.fillRect(this.x, canvas.height - this.bottom, this.width, this.bottom); // draws the bottom obstacles   
     }
 }
