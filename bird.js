d78622fc09e0cdc49a6632cad6ca73ea52e76f3e
KiedusCracknell <62589687+KiedusCracknell@users.noreply.github.com>
2022-11-09 17:10:36 +1300

    changed the sprite

--- bird.js
+++ bird.js
@@ -1,31 +1,36 @@
+const dragonSprite = new Image();
+dragonSprite.src = 'sprites/dragon.png';
 class Bird {
     constructor() {
         this.x = 150; //x position
         this.y = 200; //y position
         this.vy = 0; //y velocity
-        this.width = 20;
-        this.height = 20;
+        this.originalWidth = 475;
+        this.originalHeight = 470;
+        this.width = this.originalWidth / 10;
+        this.height = this.originalHeight / 10;
         this.weight = .5; //force that pulls player down when player isn't 'flapping'
     }
     update() {
         let curve = Math.sin(angle) * 20;
-        if (this.y > canvas.height - (this.height * 3) + curve) {
-            this.y = canvas.height - (this.height * 3) + curve;
+        if (this.y > canvas.height - (this.height * 2) + curve) {
+            this.y = canvas.height - (this.height * 2) + curve;
             this.vy = 0;
         } else { //stops the player from falling through the bottom of the canvas 
             this.vy += this.weight; //increases y velocity by weight at every frame
             this.vy * 0.9; //slow down velocity
             this.y += this.vy; //increases y position by y velocity at every frame
         }
-        if (this.y < 0 + this.height) {
-            this.y = 0 + this.height;
+        if (this.y < 0 + this.height/2) {
+            this.y = 0 + this.height/2;
             this.vy = 0;
         } //stops player from flying through the top of the canvas
-        if (spacePressed && this.y > this.height * 3) this.flap();
+         if (spacePressed && this.y > this.height) this.flap();
     } //calculate position and velocity of player character at every 
     draw() {
         ctx.fillStyle = 'hsla(' + hue + ',100%, 50%, 1'; //red player
-        ctx.fillRect(this.x, this.y, this.width, this.height); // rectangle with x position, y position, width and height
+        // ctx.fillRect(this.x, this.y, this.width, this.height); // rectangle with x position, y position, width and height
+        ctx.drawImage(dragonSprite, 0, 0, this.originalWidth, this.originalHeight, this.x, this.y, bird.width, bird.height);
     }
     flap() {
         this.vy -= 1;
